bbc35312af639575034e95c5a7d6c2b9e9ae3831
Fabien METAYER <fabien.metayer@phelma.grenoble-inp.fr>
2017-09-26 15:37:53 +0200

    v2

--- src/machine-etat.c
+++ src/machine-etat.c
@@ -1,6 +1,6 @@
-enum {INIT,COMMENT,SYM,DIR,REG,NBR,DEC,HEXA,DP,VIR,P-VIR,NL,PAR};
+enum {INIT,COMMENT,SYM,DIR,REG,NBR,DEC,HEXA,DP,VIR,PVIR,NL,PAR};
 
-int com =0
+int com =0;
 int ETAT=INIT;
 switch(ETAT);
 
@@ -18,15 +18,73 @@ case INIT:
 	else if (token[0] == ':'){
 		ETAT=DP;}
 	else if (token[0] == ';'){
-		ETAT=VIR;}
+		ETAT=PVIR;}
 	else if (isdigit(token[0]) || token[0]=='-')){
-		ETAT=PAR;}
+		ETAT=NBR;}
 	else if (token[0]=='(' || token[0]==')' ){
-		ETAT=VIR;
+		ETAT=PAR;
+	else if (
+		ETAT=NL;
 	
 	break;
 	
 case 	COMMENT:
 		com=1
 		if token == '\n'{
+		
+case DIR:
+	maillon->type="DIR";
+	maillon->token=token;
+	ajout-queue(liste,maillon);
+	break		
+	
+case REG:
+	maillon->type="REG";
+	maillon->token=token;
+	ajout-queue(liste,maillon);
+	break		
+	
+case SYM:
+	maillon->type="SYM";
+	maillon->token=token;
+	ajout-queue(liste,maillon);
+	break	
+	
+case VIR:
+	maillon->type="VIR";
+	maillon->token=token;
+	ajout-queue(liste,maillon);
+	break
+
+case DP:
+	maillon->type="DP";
+	maillon->token=token;
+	ajout-queue(liste,maillon);
+	break
+
+case PVIR:
+	maillon->type="PVIR";
+	maillon->token=token;
+	ajout-queue(liste,maillon);
+	break
+
+case PAR:
+	maillon->type="PAR";
+	maillon->token=token;
+	ajout-queue(liste,maillon);
+	break
+
+case NBR:
+	int i=0;
+	if (token[0]=='-'){
+		i=1;
+	}
+	
+
+
+
+
+
+
+		
 		
