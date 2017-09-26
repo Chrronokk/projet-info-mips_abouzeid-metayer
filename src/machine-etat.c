144fd9bf27176018cff5f663d5f969b6228652c3
Fabien METAYER <fabien.metayer@phelma.grenoble-inp.fr>
2017-09-26 16:35:31 +0200

    suite FSM

--- src/machine-etat.c
+++ src/machine-etat.c
@@ -1,7 +1,10 @@
-enum {INIT,COMMENT,SYM,DIR,REG,NBR,DEC,HEXA,DP,VIR,PVIR,NL,PAR};
-
+enum {INIT,COMMENT,SYM,DIR,REG,NBR,DEC,HEXA,DP,VIR,PVIR,NL,PAR,ERROR};
+int length= strlength(token)
 int com =0;
 int ETAT=INIT;
+int t;
+LEXEME maillon;
+LEXEME* pmaillon=
 switch(ETAT);
 
 case INIT:
@@ -22,43 +25,47 @@ case INIT:
 	else if (isdigit(token[0]) || token[0]=='-')){
 		ETAT=NBR;}
 	else if (token[0]=='(' || token[0]==')' ){
-		ETAT=PAR;
-	else if (
-		ETAT=NL;
+		ETAT=PAR;}
+	else if (token[0]='\n'){
+		ETAT=NL;}
+	else{
+		ETAT=ERROR;}
 	
 	break;
 	
-case 	COMMENT:
+case 	COMMENT: /* A COMPLETER */
 		com=1
 		if token == '\n'{
 		
 case DIR:
+	for(t=1;t<length;t++){
+		
 	maillon->type="DIR";
-	maillon->token=token;
+	maillon->lex=token;
 	ajout-queue(liste,maillon);
 	break		
 	
 case REG:
 	maillon->type="REG";
-	maillon->token=token;
+	maillon->lex=token;
 	ajout-queue(liste,maillon);
 	break		
 	
 case SYM:
 	maillon->type="SYM";
-	maillon->token=token;
+	maillon->lex=token;
 	ajout-queue(liste,maillon);
 	break	
 	
 case VIR:
 	maillon->type="VIR";
-	maillon->token=token;
+	maillon->lex=token;
 	ajout-queue(liste,maillon);
 	break
 
 case DP:
 	maillon->type="DP";
-	maillon->token=token;
+	maillon->lex=token;
 	ajout-queue(liste,maillon);
 	break
 
@@ -79,9 +86,16 @@ case NBR:
 	if (token[0]=='-'){
 		i=1;
 	}
-	
+	if(token[i]=='0'){
+		if (token[i+1]=='x'){
+			ETAT=HEXA;
+		}
+		else{
+			ETAT=DEC;
+		}
+	}
 
-
+case DEC
 
 
 
